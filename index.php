62fce453db269c77a6a73235ed829dc61d6d18f0
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-08-11 11:54:11 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Turbo Formula</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/colours.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Turbo Formula</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="http://wiki.turboformula.co.uk" target="_blank">Wiki</a></li>
+            <li><a href="http://www.turboformula.co.uk/blog" target="_blank">Blog</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Demos <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li class="dropdown-header">Projects in Progress</li>
+                <li><a href="/demos/" target="_blank">Roll-A-Ball</a></li>
+                <li class="divider"></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+<br /><br />
+
+<!-- Marketing messaging and featurettes
+================================================== -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+
+<div class="container marketing">
+
+  <hr class="featurette-divider">
+
+  <div class="featurette">
+    <img class="featurette-image img-circle pull-right" src="img/logo.png">
+    <h2 class="featurette-heading">Turbo Formula. <br /><span class="text-muted">We'll blow your mind.</span></h2>
+    <p class="text-info" style="font-size: 20px">We work hard to bring you the smoothest graphics, <br />the gameplay to end all gameplay, and the support to ice off the cake.</p>
+  </div>
+
+  <hr class="featurette-divider">
+  <h1 class="text-muted" style="text-align: center;">Meet the Team</h1>
+  <br />
+  <!-- Three columns of text below the carousel -->
+  <div class="row">
+    <div class="col-md-4 text-center">
+      <img class="img-circle" src="img/neil.png">
+      <h2 class="text-muted">Neil Wakely</h2>
+      <p class="text-info" style="font-size: 20px">Neil is one of the Coders here at Turbo Formula. He has a wide range of skills from Website Development to Electrical Repair and Beyond.</p>
+      <p><a class="btn btn-default" href="#">Neil's Bio »</a></p>
+    </div>
+    <div class="col-md-4 text-center">
+      <img class="img-circle" src="img/dean.png">
+      <h2 class="text-muted">Dean Wakely</h2>
+      <p class="text-info" style="font-size: 20px">Dean's main role here is the visual side of our group. He has a passion for AI, some would say an obsession. Python is his preferred language.</p>
+      <p><a class="btn btn-default" href="#">Dean's Bio »</a></p>
+    </div>
+    <div class="col-md-4 text-center">
+      <img class="img-circle" src="img/dave.png">
+      <h2 class="text-muted">Dave Wakely</h2>
+      <p class="text-info" style="font-size: 20px">Dave is one of the Coders here at Turbo Formula. He drinks a fair amount of coffee, eats too much cake and loves cars to a worrying degree.</p>
+      <p><a class="btn btn-default" href="#">Dave's Bio »</a></p>
+    </div>
+  </div><!-- /.row -->
+
+
+  <!-- START THE FEATURETTES -->
+
+  <hr class="featurette-divider">
+
+  <div class="featurette">
+    <img class="featurette-image pull-left" src="img/elitenation-logo.png">
+    <h2 class="featurette-heading">Elite Nation. <span class="text-muted">Sneak Peek.</span></h2>
+    <p class="text-info" style="font-size: 26px">A Hacking Simulator like no other. Free your inner demons as you venture around the open-world, destoying data and stealing money as you go.</p>
+    <p><a class="btn btn-default pull-right" href="#">Learn More »</a></p>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <div class="featurette">
+    <img class="featurette-image img-circle pull-right" src="img/logo.png">
+    <h2 class="featurette-heading">Euro Truck Simulator 2: Multiplayer. <br /><span class="text-muted">TurboTrack.</span></h2>
+    <p class="text-info" style="font-size: 26px">Virtual Trucking Companies in Euro Truck Simulator 2: Multiplayer are all the same. You write down the jobs as you go and fill out forms later online. Here at Turbo Formula, we have taken our VTC to the next level. Our servers track your progress through the Telemetry SDK to provide live data streaming directly to our secure panel. No more forms!</p>
+    <p><a class="btn btn-default pull-right" href="#">Learn More »</a></p>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <!-- /END THE FEATURETTES -->
+
+
+  <!-- FOOTER -->
+  <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Turbo Formula is a child company of <a href="http://www.gurucomputers.co.uk" target="_blank">Guru Computers Ltd.</p>
+      </div>
+    </footer>
+
+</div><!-- /.container -->
+  <!-- script references -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
